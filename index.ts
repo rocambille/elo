8373a53a9d6556d51193a505ac185993b1acac98
romaing <rocambille@gmail.com>
2022-10-08 19:05:09 +0200

    added Pool.pick method

--- index.ts
+++ index.ts
@@ -120,53 +120,132 @@ const elo = (config: Readonly<Partial<EloConfig>> = {}) => {
   };
 };
 
-class PoolHandler<T extends object> {
+class PoolHandler<T extends Object> {
+  config: EloConfig;
   player: ReturnType<typeof elo>;
 
   constructor(config: Readonly<Partial<EloConfig>>) {
+    this.config = { ...defaultConfig, ...config };
     this.player = elo(config);
   }
 
   get(target: T, prop: string | symbol, receiver: any) {
-    if (prop === "player") {
-      return (indexA: keyof T) => {
-        const playerA = this.player(target[indexA] as Object);
+    switch (prop) {
+      case "player":
+        return (indexA: keyof T) => {
+          const playerA = this.player(target[indexA] as Object);
 
-        const resolveMatch = (
-          indexB: keyof T,
-          resolver: keyof Omit<typeof playerA, "oddsAgainst" | "reset">
-        ) => {
-          const [newA, newB] = playerA[resolver](target[indexB] as Object);
-          target[indexA] = newA as unknown as T[keyof T];
-          target[indexB] = newB as unknown as T[keyof T];
-
-          return receiver;
-        };
-
-        return {
-          wins: (indexB: keyof T) => resolveMatch(indexB, "wins"),
-          ties: (indexB: keyof T) => resolveMatch(indexB, "ties"),
-          loses: (indexB: keyof T) => resolveMatch(indexB, "loses"),
-          oddsAgainst: (indexB: keyof T): number => {
-            return playerA.oddsAgainst(target[indexB] as Object);
-          },
-          reset: () => {
-            target[indexA] = playerA.reset() as unknown as T[keyof T];
+          const resolveMatch = (
+            indexB: keyof T,
+            resolver: keyof Omit<typeof playerA, "oddsAgainst" | "reset">
+          ) => {
+            const [newA, newB] = playerA[resolver](target[indexB] as Object);
+            target[indexA] = newA as unknown as T[keyof T];
+            target[indexB] = newB as unknown as T[keyof T];
 
             return receiver;
-          },
-        };
-      };
-    }
+          };
 
-    return Reflect.get(target, prop, receiver);
+          return {
+            wins: (indexB: keyof T) => resolveMatch(indexB, "wins"),
+            ties: (indexB: keyof T) => resolveMatch(indexB, "ties"),
+            loses: (indexB: keyof T) => resolveMatch(indexB, "loses"),
+            oddsAgainst: (indexB: keyof T): number => {
+              return playerA.oddsAgainst(target[indexB] as Object);
+            },
+            reset: () => {
+              target[indexA] = playerA.reset() as unknown as T[keyof T];
+
+              return receiver;
+            },
+          };
+        };
+      case "pick":
+        return (forcedMethod: string) => {
+          const methods = ["random", "matchCount", "lastPlayedAt"];
+
+          const pickRandom = (min: number, max: number) =>
+            min + Math.floor(Math.random() * (max - min + 1));
+
+          const method =
+            forcedMethod ?? methods[pickRandom(0, methods.length - 1)];
+
+          if (receiver.length === 1) {
+            throw new Error("not enough players");
+          }
+          if (receiver.length === 2) {
+            return [0, 1, method];
+          }
+
+          switch (method) {
+            case "random": {
+              const i = pickRandom(0, receiver.length - 1);
+              const j =
+                (i + pickRandom(1, receiver.length - 1)) % receiver.length;
+
+              return [i, j, method];
+            }
+            default: {
+              const defaultElo: Elo = {
+                rating: NaN,
+                lastDelta: NaN,
+                lastPlayedAt: NaN,
+                matchCount: 0,
+              };
+
+              const fromProp = (key: keyof Elo) =>
+                receiver.reduce(
+                  (
+                    [first, second, third]: Array<any>,
+                    challenger: typeof receiver[0],
+                    index: number
+                  ) => {
+                    const challengerStat =
+                      challenger[this.config.propsKey] != null
+                        ? challenger[this.config.propsKey][key] ??
+                          defaultElo[key]
+                        : defaultElo[key];
+
+                    if (challengerStat < first[key]) {
+                      return [{ index, [key]: challengerStat }, first, second];
+                    }
+                    if (challengerStat < second[key]) {
+                      return [first, { index, [key]: challengerStat }, second];
+                    }
+                    if (challengerStat < third[key]) {
+                      return [first, second, { index, [key]: challengerStat }];
+                    }
+
+                    return [first, second, third];
+                  },
+                  [
+                    { [key]: Infinity },
+                    { [key]: Infinity },
+                    { [key]: Infinity },
+                  ]
+                );
+
+              const [{ index: i }, , { index: j }] = fromProp(method as keyof Elo);
+
+              return [i, j, method];
+            }
+          }
+        };
+      default:
+        return Reflect.get(target, prop, receiver);
+    }
   }
+}
+
+interface PoolProps {
+  player: Function;
+  pick: Function;
 }
 
 const makePoolFactory =
   (config: Readonly<Partial<EloConfig>>) =>
-  <T extends object>(iterable: T): T & { player: Function } =>
-    new Proxy(iterable, new PoolHandler<T>(config)) as T & { player: Function };
+  <T extends object>(iterable: T): T & PoolProps =>
+    new Proxy(iterable, new PoolHandler<T>(config)) as T & PoolProps;
 
 export const Pool = {
   config: function (config: Readonly<Partial<EloConfig>>) {
